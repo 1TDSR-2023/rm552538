4b5b1503295247a88ba5ffb0dc9cb96518d8f010
Nathaly Oliveira <73591609+nathalyoliveira@users.noreply.github.com>
2023-05-03 22:56:17 -0300

    Button-Rgb com getById addEventListener Math

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
-var nome1 = "Petroni";
-let nome2 = "Derick";
-const nome3 = "Ruan";
+// var nome1 = "Petroni";
+// let nome2 = "Derick";
+// const nome3 = "Ruan";
 
-if(nome1 != ""){
-    let nome1 = "Geovana"
-}
+// if(nome1 != ""){
+//     let nome1 = "Geovana"
+// }
 
-console.log(nome1)
+// console.log(nome1)
+
+let botao = document.getElementById("meu-btn");
+// console.log(botao.textContent);
+
+botao.addEventListener("click", function(){
+    // console.log(this.textContent)
+    // Função matemática Math
+    // random = retorna um número aleatório entre 0 e 1
+    // floor  = arredonda um número para baixo
+    // ceil   = arredonda um número para cima
+    // round  = arredonda aleatoriamente
+
+    let r=0, g=0,b=0;
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+
+    this.setAttribute("style", `background-color:rgb(${r}, ${g}, ${b});`)
+
+});
+
+// Estudar
+// querySelector
+// querySelectorAll
+// Array
+// Estruturas de repetição
