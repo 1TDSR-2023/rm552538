7f338203dabb11765f80db2652c8d9623f2514bd
Nathaly Oliveira <73591609+nathalyoliveira@users.noreply.github.com>
2023-05-20 15:12:34 -0300

    Login - validação do usuário

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 
 
 const inputEmail = document.querySelector('#idEmail');
+const inputSenha = document.querySelector('#idPass');
 
 inputEmail.addEventListener('keyup', () => {
 
@@ -18,7 +19,6 @@ inputEmail.addEventListener('keyup', () => {
 const eyePass = document.querySelector('.fa');
 
 eyePass.addEventListener('click', () => {
-    const inputSenha = document.querySelector('#idPass');
     if(inputSenha.getAttribute('type') == 'password'){
         inputSenha.setAttribute('type', 'text');
         eyePass.setAttribute("class", "fa fa-eye-slash")
@@ -26,4 +26,92 @@ eyePass.addEventListener('click', () => {
         inputSenha.setAttribute('type', 'password');
         eyePass.setAttribute("class", "fa fa-eye")
     }
+});
+
+const botaoSubmit = document.querySelector("#btnSubmit");
+
+// Objeto
+const usuario = {
+    emailUsuario: '',
+    senhaUsuario: '',
+    nomeCompleto: ''
+};
+
+// Lista de objetos
+let listaDeUsuarios = [
+    {
+        emailUsuario: 'nat@a',
+        senhaUsuario: 'nat',
+        nomeCompleto: 'Nathaly'
+    },
+    {
+        emailUsuario: '',
+        senhaUsuario: '',
+        nomeCompleto: 'José'
+    },
+    {
+        emailUsuario: 'ma@a',
+        senhaUsuario: 'ma',
+        nomeCompleto: 'Maria'
+    },
+    {
+        emailUsuario: '',
+        senhaUsuario: '',
+        nomeCompleto: 'Henry'
+    },
+    {
+        emailUsuario: '',
+        senhaUsuario: '',
+        nomeCompleto: 'Jonh'
+    }
+];
+// listaDeUsuarios.push(usuario);
+
+botaoSubmit.addEventListener('click', () => {
+
+    // Objeto de validação
+    const userValidado = {
+        emailUsuarioValidado: inputEmail.value,
+        senhaUsuarioValidado: inputSenha.value
+    }
+
+    // Criando uma iteração com a lista de objetos para realizar 
+    // a validação com o usuarioValidado
+
+    try{
+        listaDeUsuarios.forEach((usuario) => {
+            // Realizando a validação
+            // Comparando cada objeto da lsita com o objeto montado 
+            // com os dados que vieram do formulário
+            if(userValidado.emailUsuarioValidado == usuario.emailUsuario 
+                && userValidado.senhaUsuarioValidado == usuario.senhaUsuario){
+                    userValidado.senhaUsuarioValidado = usuario.senhaUsuario;
+                    userValidado.emailUsuarioValidado = usuario.emailUsuario;
+                    throw 'Validado';
+            }
+        });
+
+        throw 'Senha ou nome de usuário inválido';
+
+    }catch(msg){
+        console.log(msg);
+        const msgStatus = document.querySelector('#msgStatus');
+        
+        if(msg == 'Validado'){
+            msgStatus.setAttribute('style', 'color:#00ff00');
+            msgStatus.innerHTML = '<span><strong>Login efetuado com sucesso</strong></span>';
+
+            // Redirec
+            setTimeout(() => {
+                window.location.href = './home.html'
+            }, 2000);
+            
+        }else{
+            msgStatus.setAttribute('style', 'color:#ff0000');
+            msgStatus.innerHTML = '<span><strong>Login não efetuado com sucesso</strong></span>';
+            
+        }
+
+    }
+
 });
